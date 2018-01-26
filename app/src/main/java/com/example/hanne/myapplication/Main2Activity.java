bc14b5dbabf4d587419693875d8afaa34cecdfb2
Niklas Carlsson <niklasc@lnxniklasc.se.axis.com>
2018-01-26 14:00:18 +0100

    hej

--- app/src/main/java/com/example/hanne/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/hanne/myapplication/Main2Activity.java
@@ -10,6 +10,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Main2Activity extends AppCompatActivity {
 
     private int frequency = 44100, channelConfiguration = AudioFormat.CHANNEL_IN_MONO, audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
@@ -25,6 +29,15 @@ public class Main2Activity extends AppCompatActivity {
         //Button audiobut = (Button) findViewById(R.id.audio);
         //audiobut.setOnClickListener();
         Log.e(MainActivity.DEBUG, "akjidjiasdas");
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+
         RecordAudio record = new RecordAudio();
         record.doInBackground();
     }
